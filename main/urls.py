9b7bec55e919dc6c2bab542cbdf649cb5ac6db9d
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-03-13 23:37:12 +0900

    project file

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html")),
 ]
 
 
